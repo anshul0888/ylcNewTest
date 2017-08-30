a9cbf2f10a3eeed6e731f76c2fe43ff7f9d53f7f
anshul0888 <anshulsharma0888@gmail.com>
2017-08-30 07:38:21 +0530

    one

--- theBernicProject/src/app/app.js
+++ theBernicProject/src/app/app.js
@@ -17,22 +17,22 @@ app.config(function($routeProvider){
 // Logo Controller
 app.controller("LogoCtrl",function($scope,$rootScope, $q, $http){
     //Logo Variable
-
+    $scope.logo = "./images/bernicLogo.png";
     // Page Title Variable
     $rootScope.title = "Your Local Cousin";
 
-    $scope.testFun = function(){
-        $http({
-            url : "http://localhost:3030/api/exp/users",
-            method : "GET"
-        }).then(function(response){
-            console.log(response);
-            $scope.logo = response.data.lg;
-        },function(response){
-            console.log("fail");
-        });
-    };
-    $scope.testFun();
+    // $scope.testFun = function(){
+    //     $http({
+    //         url : "http://localhost:3030/api/exp/users",
+    //         method : "GET"
+    //     }).then(function(response){
+    //         console.log(response);
+    //         $scope.logo = response.data.lg;
+    //     },function(response){
+    //         console.log("fail");
+    //     });
+    // };
+    // $scope.testFun();
 
 });
 
@@ -40,4 +40,8 @@ app.controller("LogoCtrl",function($scope,$rootScope, $q, $http){
 app.controller("NavBarCtrl",function($scope){
     $scope.isNavCollapsed = true;
 });
+
+app.controller("RefreshCtrl",function ($location) {
+    $location.path("/");
+})
 
