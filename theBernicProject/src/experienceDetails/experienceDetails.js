b506b26184fdb8b9c32f459073ef5a583020610d
anshul0888 <anshulsharma0888@gmail.com>
2017-09-03 14:04:06 +0530

    sept3

--- theBernicProject/src/experienceDetails/experienceDetails.js
+++ theBernicProject/src/experienceDetails/experienceDetails.js
@@ -14,9 +14,9 @@ expApp.controller('CarouselDemoCtrl', function ($scope) {
     var currIndex = 0;
 
     $scope.addSlide = function() {
-        var newWidth = 600 + slides.length + 1;
+        var newWidth = 450 + slides.length + 1;
         slides.push({
-            image: ["https://www.filepicker.io/api/file/gfB18X9uOSmqNv9k2ZmmD","https://www.filepicker.io/api/file/37Lcj8rcSuxFdeYUcOyl","https://www.filepicker.io/api/file/ypHLqUlVSYedDqN8Me9C","https://www.filepicker.io/api/file/XjXg5l5VThaaELADqKAw"],
+            image: ["./images/car1.png","./images/car2.png","https://www.filepicker.io/api/file/ypHLqUlVSYedDqN8Me9C","https://www.filepicker.io/api/file/XjXg5l5VThaaELADqKAw"],
             id: currIndex++
         });
     };
